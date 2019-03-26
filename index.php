e8241b888c5dbf5fa77ac65a3883b8d9af9a8c89
hageteruneko <39547980+hageteruneko@users.noreply.github.com>
2019-03-27 06:54:37 +0900

    Initial commit

---
+++ index.php
@@ -0,0 +1,283 @@
+<?php
+use Phalcon\Loader;
+use Phalcon\Mvc\Micro;
+use Phalcon\Di\FactoryDefault;
+use Phalcon\Db\Adapter\Pdo\Mysql as PdoMysql;
+use Phalcon\Http\Response;
+use Phalcon\Http\Request;
+
+
+// ローダーの定義
+$loader = new Loader();
+
+$loader->registerNamespaces(
+    [
+        'Store\Toys' => __DIR__ . '/models/',
+    ]
+);
+
+$loader->register();
+
+//diの構築
+$di = new FactoryDefault();
+
+$di->set(
+    "db",
+    function () {
+        return new PdoMysql(
+            [
+                "host"     => "localhost",
+                "username" => "root",
+                "password" => "",
+                "dbname"   => "test",
+            ]
+        );
+    }
+);
+
+$app = new Micro($di);
+
+// Retrieves all products
+$app->get(
+    "/api/products",
+    function () use ($app) {
+        $phql = "SELECT * FROM Store\Toys\products ORDER BY name";
+
+        $products = $app->modelsManager->executeQuery($phql);
+
+        $data = [];
+
+        foreach ($products as $product) {
+            $data[] = [
+                "id"   => $product->id,
+                "name" => $product->name,
+            ];
+        }
+
+        echo json_encode($data);
+    }
+);
+
+// Searches for products with $name in their name
+$app->get(
+    "/api/products/search/{name}",
+    function ($name) use ($app) {
+        $phql = "SELECT * FROM Store\Toys\products WHERE name LIKE :name: ORDER BY name";
+
+        $products = $app->modelsManager->executeQuery(
+            $phql,
+            [
+                "name" => "%" . $name . "%"
+            ]
+        );
+
+        $data = [];
+
+        foreach ($products as $product) {
+            $data[] = [
+                "id"   => $product->id,
+                "name" => $product->name,
+            ];
+        }
+
+        echo json_encode($data);
+    }
+);
+
+// Retrieves products based on primary key
+$app->get(
+    "/api/products/{id:[0-9]+}",
+    function ($id) use ($app) {
+        $phql = "SELECT * FROM Store\Toys\products WHERE id = :id:";
+
+        $product = $app->modelsManager->executeQuery(
+            $phql,
+            [
+                "id" => $id,
+            ]
+        )->getFirst();
+
+
+
+        // Create a response
+        $response = new Response();
+
+        if ($product === false) {
+            $response->setJsonContent(
+                [
+                    "status" => "NOT-FOUND"
+                ]
+            );
+        } else {
+            $response->setJsonContent(
+                [
+                    "status" => "FOUND",
+                    "data"   => [
+                        "id"   => $product->id,
+                        "name" => $product->name
+                    ]
+                ]
+            );
+        }
+
+        return $response;
+    }
+);
+
+// Adds a new product
+$app->post(
+    '/api/products',
+    function () use ($app) {
+        $product = $app->request->getJsonRawBody();
+
+        if(is_null($product)){
+            echo "nai";
+        }
+        
+        $phql = 'INSERT INTO Store\Toys\products (name, manual, price, image) VALUES (:name:, :manual:, :price:, :image:)';
+        
+        $status = $app->modelsManager->executeQuery(
+            $phql,
+            [
+                'name' => $product->name,
+                'manual' => $product->manual,
+                'price' => $product->price,
+                'image' => $product->image,
+            ]
+        );
+
+        // Create a response
+        $response = new Response();
+
+        // Check if the insertion was successful
+        if ($status->success() === true) {
+            // Change the HTTP status
+            $response->setStatusCode(201, "Created");
+
+            $product->id = $status->getModel()->id;
+
+            $response->setJsonContent(
+                [
+                    "status" => "OK",
+                    "data"   => $product,
+                ]
+            );
+        } else {
+            // Change the HTTP status
+            $response->setStatusCode(409, "Conflict");
+
+            // Send errors to the client
+            $errors = [];
+
+            foreach ($status->getMessages() as $message) {
+                $errors[] = $message->getMessage();
+            }
+
+            $response->setJsonContent(
+                [
+                    "status"   => "ERROR",
+                    "messages" => $errors,
+                ]
+            );
+        }
+
+        return $response;
+    }
+);
+
+// Updates products based on primary key
+$app->put(
+    "/api/products/{id:[0-9]+}",
+    function ($id) use ($app) {
+        $product = $app->request->getJsonRawBody();
+
+        $phql = "UPDATE Store\Toys\products SET name = :name:, manual = :manual:, price = :price: WHERE id = :id:";
+
+        $status = $app->modelsManager->executeQuery(
+            $phql,
+            [
+                "id"   => $id,
+                "name" => $product->name,
+                "manual" => $product->manual,
+                "price" => $product->price,
+                "image" => $product->image
+            ]
+        );
+
+        // Create a response
+        $response = new Response();
+
+        // Check if the insertion was successful
+        if ($status->success() === true) {
+            $response->setJsonContent(
+                [
+                    "status" => "OK"
+                ]
+            );
+        } else {
+            // Change the HTTP status
+            $response->setStatusCode(409, "Conflict");
+
+            $errors = [];
+
+            foreach ($status->getMessages() as $message) {
+                $errors[] = $message->getMessage();
+            }
+
+            $response->setJsonContent(
+                [
+                    "status"   => "ERROR",
+                    "messages" => $errors,
+                ]
+            );
+        }
+
+        return $response;
+    }
+);
+
+// Deletes products based on primary key
+$app->delete(
+    "/api/products/{id:[0-9]+}",
+    function ($id) use ($app) {
+        $phql = "DELETE FROM Store\Toys\products WHERE id = :id:";
+
+        $status = $app->modelsManager->executeQuery(
+            $phql,
+            [
+                "id" => $id,
+            ]
+        );
+
+        // Create a response
+        $response = new Response();
+
+        if ($status->success() === true) {
+            $response->setJsonContent(
+                [
+                    "status" => "OK"
+                ]
+            );
+        } else {
+            // Change the HTTP status
+            $response->setStatusCode(409, "Conflict");
+
+            $errors = [];
+
+            foreach ($status->getMessages() as $message) {
+                $errors[] = $message->getMessage();
+            }
+
+            $response->setJsonContent(
+                [
+                    "status"   => "ERROR",
+                    "messages" => $errors,
+                ]
+            );
+        }
+
+        return $response;
+    }
+);
+
+$app->handle();
---
+++ models/Products.php
@@ -0,0 +1,49 @@
+<?php
+
+namespace Store\Toys;
+
+use Phalcon\Mvc\Model;
+use Phalcon\Mvc\Model\Message;
+use Phalcon\Validation;
+use Phalcon\Mvc\Model\Validator\Uniqueness;
+use Phalcon\Validation\Validator\Url as UrlValidator;
+
+class products extends Model
+{
+    public function validation()
+    {
+        $validator = new Validation();
+
+        // Robot name must be unique
+        $validator->add(
+            'name',
+            new Uniqueness(
+                [
+                    'field'   => 'name',
+                    'message' => 'The product name must be unique',
+                ]
+            )
+        );
+
+        $validator->add(
+            'image', 
+            new UrlValidator(
+                [
+                    'message' => ':field must be a url'
+                ]
+            )
+        );
+
+        // Year cannot be less than zero
+        if ($this->price < 0) {
+            $this->appendMessage(
+                new Message('The year cannot be less than zero')
+            );
+        }
+
+        // Check if any messages have been produced
+        if ($this->validationHasFailed() === true) {
+            return false;
+        }
+    }
+}
